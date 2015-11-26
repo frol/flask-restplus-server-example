b50e6088811de25a9ee853d198f78c01d6f3320b
Vlad Frolov <frolvlad@gmail.com>
2015-11-26 22:31:11 +0700

    Added pagination parameters validation

--- app/extensions/api/parameters.py
+++ app/extensions/api/parameters.py
@@ -2,9 +2,18 @@
 
 from flask.ext.marshmallow import base_fields
 from flask_restplus_patched import Parameters
+from marshmallow import validate
 
 
 class PaginationParameters(Parameters):
 
-    limit = base_fields.Integer(missing=20)
-    offset = base_fields.Integer(missing=0)
+    limit = base_fields.Integer(
+        description="limit a number of items (allowed range is 1-100), default is 20.",
+        missing=20,
+        validate=validate.Range(min=1, max=100)
+    )
+    offset = base_fields.Integer(
+        description="a number of items to skip, default is 0.",
+        missing=0,
+        validate=validate.Range(min=0)
+    )
