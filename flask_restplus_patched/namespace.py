42c875587ec44e63a2338757c955b14d8a5af141
Vlad Frolov <frolvlad@gmail.com>
2016-02-26 22:28:25 +0700

    Added missed flask_restplus_patched/namespace.py

---
+++ flask_restplus_patched/namespace.py
@@ -0,0 +1,111 @@
+from functools import wraps
+
+import flask_marshmallow
+from flask_restplus import Namespace as OriginalNamespace
+from flask_restplus.utils import merge
+from webargs.flaskparser import use_args as use_webargs
+from werkzeug import cached_property, exceptions as http_exceptions
+
+from .model import Model, DefaultHTTPErrorSchema
+
+
+class Namespace(OriginalNamespace):
+
+    def _handle_api_doc(self, cls, doc):
+        if doc is False:
+            cls.__apidoc__ = False
+            return
+        ##unshortcut_params_description(doc)
+        ##handle_deprecations(doc)
+        ##for key in 'get', 'post', 'put', 'delete', 'options', 'head', 'patch':
+        ##    if key in doc:
+        ##        if doc[key] is False:
+        ##            continue
+        ##        unshortcut_params_description(doc[key])
+        ##        handle_deprecations(doc[key])
+        ##        if 'expect' in doc[key] and not isinstance(doc[key]['expect'], (list, tuple)):
+        ##            doc[key]['expect'] = [doc[key]['expect']]
+        cls.__apidoc__ = merge(getattr(cls, '__apidoc__', {}), doc)
+
+    def model(self, name=None, model=None, mask=None, **kwargs):
+        """
+        Register a model.
+        """
+        if isinstance(model, flask_marshmallow.Schema):
+            if not name:
+                name = model.__class__.__name__
+            api_model = Model(name, model, mask=mask)
+            api_model.__apidoc__ = kwargs
+            return self.add_model(name, api_model)
+        return super(Namespace, self).model(name, model, **kwargs)
+
+    def parameters(self, parameters, locations=None):
+        """
+        Register endpoint parameters.
+        """
+        def decorator(func):
+            if locations is None and parameters.many:
+                _locations = ('json', )
+            else:
+                _locations = locations
+
+            return self.doc(params=parameters)(
+                self.response(code=http_exceptions.UnprocessableEntity.code)(
+                    use_webargs(parameters, locations=_locations)(
+                        func
+                    )
+                )
+            )
+
+        return decorator
+
+    def response(self, model=None, code=200, description=None, **kwargs):
+        if model is None:
+            if code not in http_exceptions.default_exceptions:
+                raise ValueError("`model` parameter is required for code %d" % code)
+            model = self.model(
+                name='HTTPError%dSchema' % code,
+                model=DefaultHTTPErrorSchema(http_code=code)
+            )
+        if description is None:
+            if code in http_exceptions.default_exceptions:
+                description = http_exceptions.default_exceptions[code].description
+
+        def decorator(func_or_class):
+            if code in http_exceptions.default_exceptions:
+                # If the code is handled by raising an exception, it will
+                # produce a response later, so we don't need to apply a dump
+                # wrapper.
+                decorated_func_or_class = func_or_class
+            else:
+                def dump_wrapper(func):
+                    def dump_decorator(*args, **kwargs):
+                        return model.dump(func(*args, **kwargs)).data
+                    return dump_decorator
+
+                if isinstance(func_or_class, type):
+                    # Make a copy of `method_decorators` as otherwise we will
+                    # modify the behaviour of all flask-restful.Resource-based
+                    # classes
+                    func_or_class.method_decorators = (
+                        [dump_wrapper] + func_or_class.method_decorators
+                    )
+                    decorated_func_or_class = func_or_class
+                else:
+                    decorated_func_or_class = wraps(func_or_class)(dump_wrapper(func_or_class))
+
+            if isinstance(model, Model):
+                api_model = model
+            else:
+                api_model = self.model(model=model)
+
+            return self.doc(
+                responses={
+                    code: (
+                        description,
+                        [api_model] if getattr(model, 'many', False) else api_model
+                    ),
+                }
+            )(decorated_func_or_class)
+
+        return decorator
