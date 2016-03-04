6cc96712079e8c4e07b82e4504cd5056d5b1dae3
Vlad Frolov <frolvlad@gmail.com>
2016-03-04 19:37:30 +0700

    [code-quality] Replaced 'from flask.ext' import

--- flask_restplus_patched/model.py
+++ flask_restplus_patched/model.py
@@ -1,31 +1,30 @@
 from apispec.ext.marshmallow.swagger import fields2jsonschema
-from flask.ext import marshmallow
+import flask_marshmallow
 from werkzeug import cached_property
 
 from flask_restplus.model import Model as OriginalModel
 
 
-
 class SchemaMixin(object):
-   
+
     def __deepcopy__(self, memo):
         # XXX: Flask-RESTplus makes unnecessary data copying, while
         # marshmallow.Schema doesn't support deepcopyng.
         return self
 
 
-class Schema(SchemaMixin, marshmallow.Schema):
+class Schema(SchemaMixin, flask_marshmallow.Schema):
     pass
 
 
-if marshmallow.has_sqla:
-    class ModelSchema(SchemaMixin, marshmallow.sqla.ModelSchema):
+if flask_marshmallow.has_sqla:
+    class ModelSchema(SchemaMixin, flask_marshmallow.sqla.ModelSchema):
         pass
 
 
 class DefaultHTTPErrorSchema(Schema):
-    status = marshmallow.base_fields.Integer()
-    message = marshmallow.base_fields.String()
+    status = flask_marshmallow.base_fields.Integer()
+    message = flask_marshmallow.base_fields.String()
 
     def __init__(self, http_code, **kwargs):
         super(DefaultHTTPErrorSchema, self).__init__(**kwargs)
