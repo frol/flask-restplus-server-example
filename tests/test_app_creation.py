da3b8a81c913716690f55e02fb9d8c9737176aad
Vlad Frolov <frolvlad@gmail.com>
2016-10-13 23:50:26 +0300

    Added more tests for `create_app` Flask Config handling

--- tests/test_app_creation.py
+++ tests/test_app_creation.py
@@ -2,7 +2,7 @@
 # pylint: disable=missing-docstring
 import pytest
 
-from app import create_app
+from app import CONFIG_NAME_MAPPER, create_app
 
 
 def test_create_app():
@@ -21,10 +21,20 @@ def test_create_app_passing_FLASK_CONFIG_env(flask_config_name):
     create_app()
     del os.environ['FLASK_CONFIG']
 
-def test_create_app_incorrect_config():
+def test_create_app_with_conflicting_config():
     import os
     assert 'FLASK_CONFIG' not in os.environ
     os.environ['FLASK_CONFIG'] = 'production'
     with pytest.raises(AssertionError):
         create_app('development')
     del os.environ['FLASK_CONFIG']
+
+def test_create_app_with_non_existing_config():
+    with pytest.raises(KeyError):
+        create_app('non-existing-config')
+
+def test_create_app_with_broken_import_config():
+    CONFIG_NAME_MAPPER['broken-import-config'] = 'broken-import-config'
+    with pytest.raises(ImportError):
+        create_app('broken-import-config')
+    del CONFIG_NAME_MAPPER['broken-import-config']
