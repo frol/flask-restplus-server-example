f4389a9e725991ec131586dbba8b56f4e46e80a3
Vlad Frolov <frolvlad@gmail.com>
2016-10-13 23:34:56 +0300

    Added tests for new `create_app` Flask Config handling

--- tests/test_app_creation.py
+++ tests/test_app_creation.py
@@ -5,6 +5,26 @@ import pytest
 from app import create_app
 
 
-@pytest.mark.parametrize('flask_config_name', ['production', 'development'])
-def test_create_app(flask_config_name):
+def test_create_app():
+    with pytest.raises(SystemExit):
+        create_app()
+
+@pytest.mark.parametrize('flask_config_name', ['production', 'development', 'testing'])
+def test_create_app_passing_flask_config_name(flask_config_name):
     create_app(flask_config_name=flask_config_name)
+
+@pytest.mark.parametrize('flask_config_name', ['production', 'development', 'testing'])
+def test_create_app_passing_FLASK_CONFIG_env(flask_config_name):
+    import os
+    assert 'FLASK_CONFIG' not in os.environ
+    os.environ['FLASK_CONFIG'] = flask_config_name
+    create_app()
+    del os.environ['FLASK_CONFIG']
+
+def test_create_app_incorrect_config():
+    import os
+    assert 'FLASK_CONFIG' not in os.environ
+    os.environ['FLASK_CONFIG'] = 'production'
+    with pytest.raises(AssertionError):
+        create_app('development')
+    del os.environ['FLASK_CONFIG']
