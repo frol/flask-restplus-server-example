78fa2de8162fc318521d63e2bccb6f10360578c8
Vlad Frolov <frolvlad@gmail.com>
2016-11-18 17:56:34 +0200

    [tests] Improved app creation test to avoid possible side effects

--- tests/test_app_creation.py
+++ tests/test_app_creation.py
@@ -18,20 +18,14 @@ def test_create_app_passing_flask_config_name(flask_config_name):
     create_app(flask_config_name=flask_config_name)
 
 @pytest.mark.parametrize('flask_config_name', ['production', 'development', 'testing'])
-def test_create_app_passing_FLASK_CONFIG_env(flask_config_name):
-    import os
-    assert 'FLASK_CONFIG' not in os.environ
-    os.environ['FLASK_CONFIG'] = flask_config_name
+def test_create_app_passing_FLASK_CONFIG_env(monkeypatch, flask_config_name):
+    monkeypatch.setenv('FLASK_CONFIG', flask_config_name)
     create_app()
-    del os.environ['FLASK_CONFIG']
 
-def test_create_app_with_conflicting_config():
-    import os
-    assert 'FLASK_CONFIG' not in os.environ
-    os.environ['FLASK_CONFIG'] = 'production'
+def test_create_app_with_conflicting_config(monkeypatch):
+    monkeypatch.setenv('FLASK_CONFIG', 'production')
     with pytest.raises(AssertionError):
         create_app('development')
-    del os.environ['FLASK_CONFIG']
 
 def test_create_app_with_non_existing_config():
     with pytest.raises(KeyError):
