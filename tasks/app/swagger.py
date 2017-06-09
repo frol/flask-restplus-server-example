07c2a5d1eb6a12c2c5005cf245d163a0997e2c9d
Vlad Frolov <frolvlad@gmail.com>
2017-06-09 13:05:43 +0300

    [tasks] Added app.swagger.codegen command to simplify the API clients generation

--- tasks/app/swagger.py
+++ tasks/app/swagger.py
@@ -1,7 +1,10 @@
 """
 Swagger related invoke tasks
 """
+from __future__ import print_function
+
 import logging
+import os
 
 try:
     from invoke import ctask as task
@@ -10,7 +13,7 @@ except ImportError:  # Invoke 0.13 renamed ctask to task
 
 
 @task(default=True)
-def export(context):
+def export(context, output_format='json', quiet=False):
     """
     Export swagger.json content
     """
@@ -19,5 +22,56 @@ def export(context):
 
     from app import create_app
     app = create_app()
-    swagger_content = app.test_client().get('/api/v1/swagger.json').data
-    print(swagger_content.decode('utf-8'))
+    swagger_content = app.test_client().get('/api/v1/swagger.%s' % output_format).data
+    if not quiet:
+        print(swagger_content.decode('utf-8'))
+    return swagger_content
+
+
+@task
+def codegen(context, language, dry_run=False):
+    if dry_run:
+        run = print
+    else:
+        run = context.run
+
+    swagger_json_content = export(context, output_format='json', quiet=True)
+    if dry_run:
+        run(
+            "cat >./clients/%(language)s/swagger.json <<'EOF'\n%(swagger_json_content)s\nEOF"
+            % {
+                'language': language,
+                'swagger_json_content': swagger_json_content.decode('utf-8'),
+            }
+        )
+    else:
+        with open(os.path.join('.', 'clients', language, 'swagger.json'), 'wb') as swagger_json:
+            swagger_json.write(swagger_json_content)
+
+    run(
+        "docker pull 'khorolets/swagger-codegen'"
+    )
+
+    run(
+        "cd './clients/%(language)s' ;"
+        # Tar the config files to pass them into swagger-codegen docker-container.
+        "tar -c swagger.json swagger_codegen_config.json"
+        "  | docker run --interactive --rm --entrypoint /bin/sh 'khorolets/swagger-codegen' -c \""
+        # Unpack them, generate library code with these files.
+        "      tar -x ;"
+        "      java -jar '/opt/swagger-codegen/modules/swagger-codegen-cli/target/swagger-codegen-cli.jar'"
+        "        generate"
+        "          --input-spec './swagger.json'"
+        "          --lang '%(language)s'"
+        "          --output './dist'"
+        "          --config './swagger_codegen_config.json'"
+        "          --additional-properties 'packageVersion=%(version)s' >&2 ;"
+        # tar the generated code and return it.
+        "      tar -c dist\""
+        # Finally, untar library source into current directory.
+        "  | tar -x"
+        % {
+            'language': language,
+            'version': os.environ['EXAMPLE_API_VERSION'],
+        }
+    )
