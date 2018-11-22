cb496858e3300eee31d7336e52d755d28aab3551
Vlad Frolov <frolvlad@gmail.com>
2018-11-22 18:48:36 +0200

    Updated the ModelSchema export to resolve the requirement of having SQLAlchemy (which should be optional)

--- flask_restplus_patched/__init__.py
+++ flask_restplus_patched/__init__.py
@@ -1,6 +1,10 @@
 from flask_restplus import *
 from .api import Api
-from .model import Schema, ModelSchema, DefaultHTTPErrorSchema
+from .model import Schema, DefaultHTTPErrorSchema
+try:
+    from .model import ModelSchema
+except ImportError:
+    pass
 from .namespace import Namespace
 from .parameters import Parameters, PostFormParameters, PatchJSONParameters
 from .swagger import Swagger
