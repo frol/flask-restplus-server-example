e5867b00f313b22e7dcec096fa3892321fbd426f
Jason Parham <bluemellophone@gmail.com>
2020-08-17 11:02:52 -0700

    Pending

--- app/modules/assets/models.py
+++ app/modules/assets/models.py
@@ -11,7 +11,7 @@ from app.extensions import db, TimestampViewed
 import uuid
 
 
-class Asset(db.Model, TimestampViewed):
+class Submission(db.Model, TimestampViewed):
     """
     Assets database model.
     """
@@ -20,7 +20,51 @@ class Asset(db.Model, TimestampViewed):
         db.GUID, default=uuid.uuid4, primary_key=True
     )  # pylint: disable=invalid-name
 
-    code = db.Column(db.String(length=64), nullable=False)
+    _hash =
+
+    title =
+    description =
+    metadata =
+
+    submitter_guid =
+    submitter =
+
+    owner_guid =
+    owner =
+
+    def __repr__(self):
+        return (
+            '<{class_name}('
+            'guid={self.guid}, '
+            'file="{self.code}{self.ext}"'
+            ')>'.format(class_name=self.__class__.__name__, self=self)
+        )
+
+    @property
+    def absolute_filepath(self):
+        asset_path = current_app.config.get('SUBMISSION_DATABASE_PATH', None)
+        asset_filname = '%s%s' % (self.code, self.ext,)
+        asset_filepath = os.path.join(asset_path, asset_filname,)
+
+        asset_filepath = os.path.abspath(asset_filepath)
+        assert os.path.exists(asset_filepath)
+        return asset_filepath
+
+
+class Asset(db.Model, TimestampViewed):
+    """
+    Assets database model.
+    """
+
+    guid = db.Column(
+        db.GUID, default=uuid.uuid4, primary_key=True
+    )  # pylint: disable=invalid-name
+    description =
+    metadata =
+
+    submission_guid =
+    submission
+
     ext = db.Column(db.String(length=5), nullable=False)
 
     def __repr__(self):
