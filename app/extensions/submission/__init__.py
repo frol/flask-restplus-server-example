c8e8dc1fada3df48b1cc5ff606de06983ca0e7d2
Jason Parham <bluemellophone@gmail.com>
2020-09-11 10:39:02 -0700

    WIP

--- app/extensions/submission/__init__.py
+++ app/extensions/submission/__init__.py
@@ -8,6 +8,7 @@ import logging
 
 from flask import current_app, request, session, render_template  # NOQA
 from flask_login import current_user  # NOQA
+import gitlab
 
 import pytz
 
@@ -23,22 +24,32 @@ PST = pytz.timezone('US/Pacific')
 
 
 class SubmissionManager(object):
-    def __init__(self, app, pre_initialize=False, *args, **kwargs):
+    def __init__(self, app, pre_initialize=True, *args, **kwargs):
         super(SubmissionManager, self).__init__(*args, **kwargs)
         self.initialized = False
 
         self.app = app
-
         app.sub = self
+
+        self.gl = None
 
         if pre_initialize:
             self.ensure_initialed()
 
     def ensure_initialed(self):
-        remote_uri = self.app.config.get('GIT_REMOTE_URI', None)
-        remote_personal_access_token = self.app.config.get('GIT_REMOTE_LOGIN_PAT', None)
+        if not self.initialized:
+            assert self.gl is None
+            logging.info('Logging into Submission GitLab...')
+            remote_uri = self.app.config.get('GITLAB_REMOTE_URI', None)
+            remote_personal_access_token = self.app.config.get(
+                'GITLAB_REMOTE_LOGIN_PAT', None
+            )
 
-        print(remote_uri, remote_personal_access_token)
+            self.gl = gitlab.Gitlab(
+                remote_uri, private_token=remote_personal_access_token
+            )
+            self.gl.auth()
+            logging.info('Logged in: %r' % (self.gl,))
 
 
 def init_app(app, **kwargs):
