fc5834403cd568b1aca1e98852d06437a42798a3
Jon Van Oast <jon@wildme.org>
2020-09-27 09:11:17 +0000

    start of some tests for submissions/streamlined

--- tests/modules/submission/resources/test_upload_file.py
+++ tests/modules/submission/resources/test_upload_file.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=missing-docstring
 import json
+import io
+import os
+import filecmp
 
 
 def test_create_open_submission(flask_app_client, regular_user, db):
@@ -43,3 +46,53 @@ def test_create_open_submission(flask_app_client, regular_user, db):
         # Restore original state
         with db.session.begin():
             db.session.delete(temp_submission)
+
+def test_submission_streamlined(flask_app_client, regular_user, db):
+    # pylint: disable=invalid-name
+    try:
+
+        from app.modules.submissions.models import Submission, SubmissionMajorType
+
+        test_major_type = SubmissionMajorType.test
+
+        with flask_app_client.login(regular_user, auth_scopes=('submissions:write',)):
+            response = flask_app_client.post(
+                '/api/v1/submissions/streamlined',
+                data=dict(
+                    major_type=test_major_type,
+                    title='Test Submission',
+                    description='Test Submission (streamlined)',
+                    files=[
+                        (open('tests/submissions/test-000/fluke.jpg', 'rb'), 'fluke.jpg'),
+                        (open('tests/submissions/test-000/zebra.jpg', 'rb'), 'zebra.jpg'),  #FIXME unclosed file warning
+                    ]
+                )
+            )
+
+        temp_submission = Submission.query.get(response.json['guid'])
+
+        assert response.status_code == 200
+        assert response.content_type == 'application/json'
+        assert isinstance(response.json, dict)
+        assert set(response.json.keys()) >= {
+            'guid',
+            'commit',
+            'major_type',
+            'owner_guid',
+        }
+
+        repo, project = temp_submission.init_repository()   #is this the right way to get (local) repo ??
+        #### compares file in local repo
+        assert filecmp.cmp(repo.working_tree_dir + '/fluke.jpg', 'tests/submissions/test-000/fluke.jpg')
+        assert filecmp.cmp(repo.working_tree_dir + '/zebra.jpg', 'tests/submissions/test-000/zebra.jpg')
+        ### TODO other tests?  confirm commit, confirm remote has files, etc?????  (once i get push working!)
+
+        assert temp_submission.commit is None
+        assert temp_submission.major_type == test_major_type
+    except Exception as ex:
+        raise ex
+    finally:
+        # Restore original state
+        with db.session.begin():
+            db.session.delete(temp_submission)
+
