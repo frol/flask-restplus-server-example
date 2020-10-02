b300492b1e56def9e866b6bd11e6fce02ba0f6e5
Jon Van Oast <jon@wildme.org>
2020-10-02 20:40:45 +0000

    better testing and better python

--- tests/modules/submission/resources/test_upload_file.py
+++ tests/modules/submission/resources/test_upload_file.py
@@ -4,6 +4,8 @@ import json
 import io
 import os
 import filecmp
+import config
+from os.path import join, basename
 
 
 def test_create_open_submission(flask_app_client, regular_user, db):
@@ -54,6 +56,12 @@ def test_submission_streamlined(flask_app_client, regular_user, db):
         from app.modules.submissions.models import Submission, SubmissionMajorType
 
         test_major_type = SubmissionMajorType.test
+        project_root = join(config.TestingConfig.PROJECT_ROOT, 'tests', 'submissions', 'test-000')
+        test_image_list = ['zebra.jpg', 'fluke.jpg']
+        files=[
+            _upload_content(join(project_root, filename))
+            for filename in test_image_list
+        ]
 
         with flask_app_client.login(regular_user, auth_scopes=('submissions:write',)):
             response = flask_app_client.post(
@@ -62,12 +70,11 @@ def test_submission_streamlined(flask_app_client, regular_user, db):
                     major_type=test_major_type,
                     title='Test Submission',
                     description='Test Submission (streamlined)',
-                    files=[
-                        (open('tests/submissions/test-000/fluke.jpg', 'rb'), 'fluke.jpg'),
-                        (open('tests/submissions/test-000/zebra.jpg', 'rb'), 'zebra.jpg'),  #FIXME unclosed file warning
-                    ]
+                    files=files,
                 )
             )
+        # since we passed file descriptors to files we need to close them now
+        [f[0].close() for f in files]
 
         temp_submission = Submission.query.get(response.json['guid'])
 
@@ -81,10 +88,10 @@ def test_submission_streamlined(flask_app_client, regular_user, db):
             'owner_guid',
         }
 
-        repo, project = temp_submission.init_repository()   #is this the right way to get (local) repo ??
+        repo, project = temp_submission.init_repository()   # is this the right way to get (local) repo ??
         #### compares file in local repo
-        assert filecmp.cmp(repo.working_tree_dir + '/fluke.jpg', 'tests/submissions/test-000/fluke.jpg')
-        assert filecmp.cmp(repo.working_tree_dir + '/zebra.jpg', 'tests/submissions/test-000/zebra.jpg')
+        for filename in test_image_list:
+            assert filecmp.cmp(join(repo.working_tree_dir, filename), join(project_root, filename))
         ### TODO other tests?  confirm commit, confirm remote has files, etc?????  (once i get push working!)
 
         assert temp_submission.commit is None
@@ -95,4 +102,9 @@ def test_submission_streamlined(flask_app_client, regular_user, db):
         # Restore original state
         with db.session.begin():
             db.session.delete(temp_submission)
+
+
+def _upload_content(path):
+    bname=basename(path)
+    return open(path, 'rb'), bname
 
