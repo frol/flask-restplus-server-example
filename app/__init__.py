53a3a156cc9df414537860ed677bd0cc98dd2271
Misha <ulianich_mihail@ukr.net>
2023-06-21 17:30:54 +0300

    Refactor flask_config_name evaluation (#155)
    Simplify branching

--- app/__init__.py
+++ app/__init__.py
@@ -28,19 +28,16 @@ def create_app(flask_config_name=None, **kwargs):
     app = Flask(__name__, **kwargs)
 
     env_flask_config_name = os.getenv('FLASK_CONFIG')
-    if not env_flask_config_name and flask_config_name is None:
-        flask_config_name = 'local'
-    elif flask_config_name is None:
-        flask_config_name = env_flask_config_name
-    else:
-        if env_flask_config_name:
-            assert env_flask_config_name == flask_config_name, (
-                "FLASK_CONFIG environment variable (\"%s\") and flask_config_name argument "
-                "(\"%s\") are both set and are not the same." % (
-                    env_flask_config_name,
-                    flask_config_name
-                )
+    if flask_config_name is None:
+        flask_config_name = env_flask_config_name or 'local'
+    elif env_flask_config_name:
+        assert env_flask_config_name == flask_config_name, (
+            "FLASK_CONFIG environment variable (\"%s\") and flask_config_name argument "
+            "(\"%s\") are both set and are not the same." % (
+                env_flask_config_name,
+                flask_config_name
             )
+        )
 
     try:
         app.config.from_object(CONFIG_NAME_MAPPER[flask_config_name])
